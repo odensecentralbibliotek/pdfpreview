dd9a357c913b6c124c2d67963fe511eacdd03dd6
impara <impara1@gmail.com>
2018-04-12 15:52:44 +0200

    Fixes wysiwyg on page refresh

--- pdfpreview.js
+++ pdfpreview.js
@@ -8,36 +8,15 @@
     Drupal.behaviors.pdfpreview = {
         attach: function (context, settings) {
 
-            // can access setting from 'settings' or 'Drupal.settings';
-            var path = Drupal.settings.pdfpreview.path;
-            var filename = Drupal.settings.pdfpreview.filename;
-            var fid = Drupal.settings.pdfpreview.fid;
-            var arrayLength = fid.length;
-
-            for (var i = 0; i < arrayLength; i++) {
-                $('#pdfpreview-' + fid[i]).hide();
-                var test = $('<span id="file-' + fid[i] + '"><img class="file-icon" alt="" title="application/pdf" src="/modules/file/icons/application-pdf.png"> <a href=' + path[i] + ' type="application/pdf;">' + filename[i] + '</a></span>').mouseover(function () {
-                    $('#pdfpreview-' + fid[i]).show();
+                $('div[id^=pdfpreview-').hide();
+                $('[id^=file-').mouseover(function () {
+                var numeric_part = $( this ).attr( 'id' ).substr( 5 );
+                 $('#pdfpreview-'+ numeric_part).show();
                 }).mouseout(function () {
-                    $('#pdfpreview-' + fid).hide();
-                    $('#file-' + fid[i]).show();
-                });
-                ;
-
-                test.mouseover(function () {
-                    $(this).next().show();
-                }).mouseout(function () {
-                    $(this).next().hide();
-                    $(this).show();
+                    var numeric_part = $( this ).attr( 'id' ).substr( 5 );
+                   $('#pdfpreview-'+ numeric_part).hide();
                 });
 
-                if ($('#file-' + fid[i]).length) {
-                    //do something if elem is present
-                } else {
-                    test.insertBefore('#pdfpreview-' + fid[i]);
-                }
-
-            };
         }
     };
 
