4cdc912aa8ff61b4309342cb3d19d208f07d9466
impara <impara1@gmail.com>
2018-04-25 14:00:28 +0200

    Added preview for images

--- pdfpreview.js
+++ pdfpreview.js
@@ -8,13 +8,36 @@
     Drupal.behaviors.pdfpreview = {
         attach: function (context, settings) {
 
-                $('div[id^=pdfpreview-').hide();
-                $('[id^=file-').mouseover(function () {
+                $('div[id^=pdfpreview-]').hide();
+                $('[id^=file-]').mouseover(function () {
                 var numeric_part = $( this ).attr( 'id' ).substr( 5 );
-                 $('#pdfpreview-'+ numeric_part).show();
+                var styles = {
+                   position: "absolute",
+                   transform: "translate(135%, -30%)",
+                   zIndex: "10"
+                };
+                $('#pdfpreview-'+ numeric_part).css(styles).show();
                 }).mouseout(function () {
                     var numeric_part = $( this ).attr( 'id' ).substr( 5 );
                    $('#pdfpreview-'+ numeric_part).hide();
+                });
+                
+                $('a[type^=image]').one('mouseover', function () {
+                var image = $( this ).attr( 'href' );
+                $( this ).append('<img class="theImg" src="' + image + '" />');               
+                });
+                
+                $('a[type^=image]').mouseover(function () {
+                   var styles = {
+                   position: "absolute",
+                   transform: "translate(100%, -30%)",
+                   maxWidth: "420px",
+                   maxHeight: "420px",
+                   zIndex: "10"
+                };
+                $(this).find('img').css(styles).show();
+                }).mouseout(function () {
+                    $(this).find('img').hide();
                 });
 
         }
